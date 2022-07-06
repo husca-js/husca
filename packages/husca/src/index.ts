4002bce20a8d02c5bdaca613218925961b6f89d3
geekact <fanwenhua1990@gmail.com>
2022-07-06 23:20:27 +0800

    feat(husca): export more api

--- packages/husca/src/index.ts
+++ packages/husca/src/index.ts
@@ -5,6 +5,8 @@ export {
   ConsoleAppOptions,
   WebCtx,
   ConsoleCtx,
+  WebRequest,
+  WebResponse,
 } from './app';
 export {
   createSlot,
@@ -16,7 +18,10 @@ export {
   WebSlot,
   ConsoleSlot,
   MixedSlot,
+  WebSlotManager,
+  ConsoleSlotManager,
 } from './slot';
 export { Router, RouterOptions, Commander, CommanderOptions } from './router';
 export { Validator, rule, validate } from './validators';
-export { Next} from './utils/compose'
+export { Next } from './utils/compose';
+export { HttpError } from 'http-errors';
